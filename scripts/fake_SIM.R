d376ead5c248fd17202066e617aa8db52b06ece7
Erik Santos <erikluan@alumni.usp.br>
2025-06-02 00:10:36 -0300

    compilando dados do SIM

--- scripts/fake_SIM.R
+++ scripts/fake_SIM.R
@@ -1,10 +1,20 @@
 library(microdatasus)
 library(dplyr)
+
+## ----------------------------------
+## Baixando a base de óbitos do SIM
+## ----------------------------------
 # base de óbitos
 sim <- fetch_datasus(
   year_start = 2016, 
-  year_end = 2022, 
+  year_end = 2023, 
   uf = "PB", 
   information_system = "SIM-DO")
 
-sim |> select(DTNASC) |> vitallinkage::ajuste_data(tipo_data=1)
+
+## ----------------------------------
+## Upload no banco de dados
+## ----------------------------------
+source("R/conectar.R")
+con_fake <- conectar("fake")
+dbWriteTable(con_fake, "sim", sim, overwrite = TRUE, row.names = FALSE)
